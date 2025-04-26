994c933ce9cf93b32c986f3ce5c1bb04e59022b7
Krishna Jeena <91063845+thekrishnajeena@users.noreply.github.com>
2025-04-26 10:06:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY = "AIzaSyCStBMjtNJ_2_FUIgWz5YS9Oct0_DSoVMw" #os.getenv("GEMINI_API_KEY")
 GEMINI_ENDPOINT = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent"
 
 @app.route("/quote", methods=["GET"])
