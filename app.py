97feedd295df8d97a45c8d88b142c7300626dc07
Krishna Jeena <91063845+QAEYE@users.noreply.github.com>
2025-04-26 09:50:22 +0530

    serving service 2

--- app.py
+++ app.py
@@ -32,3 +32,7 @@ def get_quote():
         return jsonify({"quote": quote})
     else:
         return jsonify({"error": "Failed to fetch quote"}), 500
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=10000)
